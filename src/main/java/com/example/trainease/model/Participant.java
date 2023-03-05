72489cb8f17c0e6b80988f75b2727aaf4e1068a5
Banitongwa David <davidbanitongwa@gmail.com>
2023-03-05 21:44:59 +0100

    ProfilDAO and Participant class

--- src/main/java/com/example/trainease/model/Participant.java
+++ src/main/java/com/example/trainease/model/Participant.java
@@ -20,5 +20,43 @@ public class Participant {
         this.code_profil = code_profil;
     }
 
+    public int getMatricule_participant() {
+        return matricule_participant;
+    }
 
+    public void setMatricule_participant(int matricule_participant) {
+        this.matricule_participant = matricule_participant;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public Date getDate_naissance() {
+        return date_naissance;
+    }
+
+    public void setDate_naissance(Date date_naissance) {
+        this.date_naissance = date_naissance;
+    }
+
+    public int getCode_profil() {
+        return code_profil;
+    }
+
+    public void setCode_profil(int code_profil) {
+        this.code_profil = code_profil;
+    }
 }
