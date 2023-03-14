800b2eceaa2b42dc42e8d632e35b5d26a4aec1d0
Banitongwa David <davidbanitongwa@gmail.com>
2023-03-14 22:51:42 +0100

    update home for simple user, liste formation, formateur and participant

---
+++ src/main/java/com/example/trainease/servlet/ServletStatistique.java
@@ -0,0 +1,25 @@
+package com.example.trainease.servlet;
+
+import com.example.trainease.dao.FormateurDAO;
+import com.example.trainease.dao.FormationDAO;
+import com.example.trainease.dao.ParticipantDAO;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "ServletStatistique", value = "/ServletStatistique")
+public class ServletStatistique extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
--- src/main/webapp/liste_participants.jsp
+++ src/main/webapp/liste_participants.jsp
@@ -8,9 +8,33 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Title</title>
+    <title>Liste des participants</title>
 </head>
 <body>
+
+<div class="container">
+    <h2>Liste des participants</h2>
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th>Nom</th>
+            <th>Prénom</th>
+            <th>Email</th>
+            <th>Téléphone</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        <tr>
+            <td>John</td>
+            <td>Doe</td>
+            <td>john@example.com</td>
+            <td>555-1234</td>
+        </tr>
+
+        </tbody>
+    </table>
+</div>
 
 </body>
 </html>
